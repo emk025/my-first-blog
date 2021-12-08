54bb5f289163cca11e6b07fcfcf080b8566a0565
Emily Kim <ekim010@citymail.cuny.edu>
2021-12-07 22:40:31 -0500

    book model

--- blog/models.py
+++ blog/models.py
@@ -16,3 +16,18 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+class Book(models.Model):
+    author = models.CharField(max_length=200)
+    title = models.CharField(max_length=200)
+    summary = models.TextField
+    genre = models.CharField(max_length=200)
+    publish_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.publish_date = timezone.now()
+        self.save()
+        
+    def __str__(self):
+        return self.title
+    
