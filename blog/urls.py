93ae20e658fb96648a1382068277865152397d01
Emily Kim <ekim010@citymail.cuny.edu>
2021-12-22 17:13:57 -0500

    changed the HTML for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,13 @@
 from django.urls import include, path
 from rest_framework import routers
 from . import views
-from blog.views import PostViewSet
+from blog.views import PostViewSet, post_list
 
 router = routers.DefaultRouter()
 router.register(r'post', PostViewSet)
 
 urlpatterns = [
   path('', include(router.urls)),
-  path('', views.post_list, name='post_list'),
+  path('post_list', views.post_list, name='post_list'),
+
 ]
